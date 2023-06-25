55594e1d49a099f70530bf040dc75638a022586b
darnavanand <78216181+darnavanand@users.noreply.github.com>
2023-06-25 13:00:36 +0530

    Add files via upload

---
+++ scripit.js
@@ -0,0 +1,6 @@
+function openPopup(){
+    document.getElementById("popup").classList.add("open-popup");
+}
+function closePopup(){
+    document.getElementById("popup").classList.remove("open-popup");
+}
